1e55d3473c40e681aa46cfdadb4042c662e24737
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-06-07 19:26:10 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+echo 'This will configure and bootstrap chiaki (PS4 Remote Play) client';
 sudo apt-get install cmake ffmpeg. h264. qt5-default qttools5-dev-tools libqt5svg5-dev qtmultimedia5-dev opus. libproto. protobuf. -y;
 git clone https://github.com/thestr4ng3r/chiaki.git --recursive;
 cd chiaki;
