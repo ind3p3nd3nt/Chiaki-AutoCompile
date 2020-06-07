77ce43363ce75af2ef1a5e08f105347452524d52
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-06-07 19:19:30 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/bash
 sudo apt-get install cmake ffmpeg. h264. qt5-default qttools5-dev-tools libqt5svg5-dev qtmultimedia5-dev opus. libproto. protobuf. -y;
 git clone https://github.com/thestr4ng3r/chiaki.git --recursive;
 cd chiaki;
