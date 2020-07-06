18c913703551c77eb982914315e352493553cd16
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-07-06 00:26:49 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+#This script works well with Debian (sid)
 echo 'This will configure and bootstrap chiaki (PS4 Remote Play) client';
 sudo apt-get install cmake libavutil-dev libswresample-dev libswscale-dev python3 python3-pip libssl-dev nanopb libavcodec-dev ffmpeg protobuf-compiler libopengl-dev h264. libsdl2-dev qt5-default qttools5-dev-tools libqt5svg5-dev qtmultimedia5-dev libopus-dev libprotobuf-dev -y;
 sudo pip3 install protobuf;
