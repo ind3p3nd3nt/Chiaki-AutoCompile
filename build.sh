8fe04a388e75c8ea0721c6c26b8c1c6ad5d05d36
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-07-06 00:26:03 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 echo 'This will configure and bootstrap chiaki (PS4 Remote Play) client';
-sudo apt-get install cmake libavutil-dev libswresample-dev libswscale-dev libssl-dev nanopb libavcodec-dev ffmpeg protobuf-compiler libopengl-dev h264. libsdl2-dev qt5-default qttools5-dev-tools libqt5svg5-dev qtmultimedia5-dev libopus-dev libprotobuf-dev -y;
+sudo apt-get install cmake libavutil-dev libswresample-dev libswscale-dev python3 python3-pip libssl-dev nanopb libavcodec-dev ffmpeg protobuf-compiler libopengl-dev h264. libsdl2-dev qt5-default qttools5-dev-tools libqt5svg5-dev qtmultimedia5-dev libopus-dev libprotobuf-dev -y;
+sudo pip3 install protobuf;
 git clone https://github.com/thestr4ng3r/chiaki.git --recursive;
 cd chiaki;
 cmake . && make -j8 && sudo make install;
